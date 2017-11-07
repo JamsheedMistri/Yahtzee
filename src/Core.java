4e37e1c53321f2fedc9187f140fe2dd8c73f2ba0
Jamsheed Mistri <jmistri7@gmail.com>
2017-11-07 13:56:55 -0800

    Fix bug

--- src/Core.java
+++ src/Core.java
@@ -2,8 +2,10 @@
 public class Core {
 	public static boolean confirm(String affirmative, String negative) {
 		String s = TextIO.getlnString();
-		System.out.println("Invalid input! Please enter \"" + affirmative + "\" or \"" + negative + "\": ");
-		while (!s.equals(affirmative) && !s.equals(negative)) s = TextIO.getlnString();
+		while (!s.equals(affirmative) && !s.equals(negative)) {
+			System.out.println("Invalid input! Please enter \"" + affirmative + "\" or \"" + negative + "\": ");
+			s = TextIO.getlnString();
+		}
 		if (s.equals(affirmative)) return true;
 		return false;
 	}
